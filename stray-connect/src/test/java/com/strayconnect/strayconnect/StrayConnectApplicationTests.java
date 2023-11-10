a54caa860c79853991a8551cbfbacaf160c9ec15
Chitranssh <saxenaayush2052001@gmail.com>
2023-11-10 10:44:03 +0530

    Initialized Spring Boot Project

---
+++ stray-connect/src/test/java/com/strayconnect/strayconnect/StrayConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strayconnect.strayconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrayConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
